9b84ee7d2dcdb50e0fc632fdc618ccb9ebef66c7
comradegenrr <naplnp@126.com>
2023-10-05 02:29:00 +0800

    合理化数据库链接

--- dboption/dbOption.go
+++ dboption/dbOption.go
@@ -21,12 +21,9 @@ var (
 // 1.建立连接
 func DbInit() *mongo.Collection {
 	log := logutil.GetLog()
-	defer func() {
-		if dbErr := recover(); dbErr != nil {
-			log.Err(nil).Msg("链接数据库失败")
-		}
-	}()
+
 	if client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://107.172.86.242:27017").SetConnectTimeout(5*time.Second)); err != nil {
+		log.Err(err).Msg("链接数据库失败")
 		panic("链接数据库失败")
 	}
 	//2.选择数据库 my_db
